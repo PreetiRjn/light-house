22960e462f906a3507363ced921f5d4bade934b3
preeti <preeti.r08@gmail.com>
2020-09-11 17:48:01 +0530

    lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,7 @@ module.exports = {
       },
       assert: {
         assertions: {
-          'categories:performance': ['warn', {minScore: 1}],
+          'categories:performance': ['error', {minScore: 1}],
           'categories:accessibility': ['error', {minScore: 1}]
         }
       },
